6e096c3ec9ac79d9a7b5eab4502ea6cbd6e59cac
dfsmarsi <douglassmarsi10@gmail.com>
2022-06-08 11:42:21 -0300

    upando

---
+++ SigecomTesteUI/CadastroCliente.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System.Threading;
+
+namespace SigecomTesteUI
+{
+    [TestClass]
+    public class TesteDev : AppiumDriver
+    {
+        [TestInitialize]
+        public void Logar()
+        {
+            Setup();
+            DigitarNoCampo("txtUsuario", "Douglas");
+            DigitarNoCampoEnter("txtSenha", "123");
+            Thread.Sleep(20000);
+            TrocarJanela();
+            Verificar("SIGECOM - Sistema de Gestão Comercial - NFCE", "SIGECOM - Sistema de Gestão Comercial - NFCE");
+        }
+        [TestMethod]
+        public void CadastrarProdutoNovo()
+        {
+            ClicarBotao("Cadastro");
+        }
+    }
+}
---
+++ SigecomTesteUI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SigecomTesteUI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SigecomTesteUI")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1553a2c4-9575-47b5-8cd1-567e8200d49e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
