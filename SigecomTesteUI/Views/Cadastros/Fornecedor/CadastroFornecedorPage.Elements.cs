5bfa6f4eb4f62f6c7488711f55b7879b84534e25
dfsmarsi <douglassmarsi10@gmail.com>
2022-07-13 18:02:04 -0300

    Criando elementos da page Fornecedor

--- SigecomTesteUI/Views/Cadastros/Fornecedor/CadastroFornecedorPage.Elements.cs
+++ SigecomTesteUI/Views/Cadastros/Fornecedor/CadastroFornecedorPage.Elements.cs
@@ -4,13 +4,12 @@ namespace SigecomTesteUI.Views.Cadastros.Fornecedor
 {
     public partial class CadastroFornecedorPage : Base
     {
-        //DoubleClickBotao("Cadastro");
         public WindowsElement Menu => WindowsDriver.FindElementByName("Cadastro");
-        //ClicarBotaoName("Fornecedores");
-        //ClicarBotaoName("F2 - Novo");
-        //DigitarNoCampo("txtNome", "FUSKAS TELEMARKETING");
-        //DigitarNoCampo("txtCPF", "39920109029");
-        //DigitarNoCampo("txtRG", "111111111");
+        public WindowsElement BotaoCadastroFornecedor => WindowsDriver.FindElementByName("Fornecedores");
+        public WindowsElement NovoCadastroFornecedor => WindowsDriver.FindElementByName("F2 - Novo");
+        public WindowsElement NomeFornecedor => WindowsDriver.FindElementByAccessibilityId("txtNome");
+        public WindowsElement CpfFornecedor => WindowsDriver.FindElementByAccessibilityId("txtCPF");
+        public WindowsElement RgFornecedor => WindowsDriver.FindElementByAccessibilityId("txtRG");
         //DigitarNoCampoEnter("txtCEP", "15700082");
         //Thread.Sleep(TimeSpan.FromSeconds(3));
         //DigitarNoCampo("txtNumero", "123");
