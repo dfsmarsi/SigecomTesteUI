2bfea304a4c3496b49eef6fd4e1462fb67aeb5b3
Takaki <takaki@sistemasbr.net>
2022-11-16 11:43:26 -0300

    criação(Models): Criando models dos testes automatizados. SIG-Autofac

---
+++ SigecomTestesUI/Sigecom/Cadastros/Pessoas/Colaborador/Model/CadastroDeColaboradorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SigecomTestesUI.Sigecom.Cadastros.Pessoas.Colaborador.Model
+{
+    public static class CadastroDeColaboradorModel
+    {
+        public static string BotaoMenu => "Cadastro";
+        public static string BotaoSubMenu => "Colaboradores";
+        public static string TelaCadastroColaborador => "Cadastro de colaboradores";
+        public static string BotaoNovo => "F2 - Novo";
+        public static string BotaoGravar => "F5 - Gravar";
+        public static string BotaoPesquisar => "F9 - Pesquisar";
+        public static string ElementoTipoPessoa => "cbxPessoaClassificacao";
+        public static string ElementoNome => "txtNome";
+        public static string ElementoCpf => "txtCPF";
+        public static string ElementoCep => "txtCEP";
+        public static string ElementoNumero => "txtNumero";
+    }
+}
---
+++ SigecomTestesUI/Sigecom/Cadastros/Pessoas/Fornecedor/Model/CadastroDeFornecedorModel.cs
@@ -0,0 +1,17 @@
+﻿namespace SigecomTestesUI.Sigecom.Cadastros.Pessoas.Fornecedor.Model
+{
+    public class CadastroDeFornecedorModel
+    {
+        public static string BotaoMenu => "Cadastro";
+        public static string BotaoSubMenu => "Fornecedores";
+        public static string TelaCadastroFornecedor => "Cadastro de fornecedores";
+        public static string BotaoNovo => "F2 - Novo";
+        public static string BotaoGravar => "F5 - Gravar";
+        public static string BotaoPesquisar => "F9 - Pesquisar";
+        public static string ElementoTipoPessoa => "cbxPessoaClassificacao";
+        public static string ElementoNome => "txtNome";
+        public static string ElementoCpf => "txtCPF";
+        public static string ElementoCep => "txtCEP";
+        public static string ElementoNumero => "txtNumero";
+    }
+}
