3ac88e1c7ec9b1a488f2c90356a054aad905117c
Takaki <takaki@sistemasbr.net>
2022-12-07 17:58:33 -0300

    criação(EditarProduto): Criando primeiro teste do editar produto simples. SIG-EdicaoDeProduto

---
+++ SigecomTestesUI/Sigecom/Cadastros/Produtos/EditarProduto/Model/EditarProdutoNovoSimplesModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SigecomTestesUI.Sigecom.Cadastros.Produtos.EditarProduto.Model
+{
+    public class EditarProdutoNovoSimplesModel
+    {
+        public static string NomeDoProduto => "PRODUTO SIMPLES AlTERADO";
+        public static string CategoriaDoProduto => "PRODUTO";
+        public static string NomeFinalDoProduto => "PRODUTO SIMPLES AlTERADO";
+        public static string UnidadeDoProduto => "UN";
+        public static string CodigoInternoDoProduto => "int";
+        public static string CustoDoProduto => "2,00";
+        public static string MarkupDoProduto => "10,00";
+        public static string PrecoVendaDoProduto => "2,00";
+        public static string ReferenciaDoProduto => "ref 2";
+        public static string NcmDoProduto => "22030000";
+    }
+}
---
+++ SigecomTestesUI/Sigecom/Cadastros/Produtos/EditarProduto/Teste/EdicaoDeProdutoSimplesTeste.cs
@@ -0,0 +1,40 @@
+﻿using Autofac;
+using NUnit.Allure.Attributes;
+using NUnit.Framework;
+using SigecomTestesUI.ControleDeInjecao;
+using SigecomTestesUI.Services;
+using SigecomTestesUI.Sigecom.Cadastros.Produtos.Model;
+using System;
+
+namespace SigecomTestesUI.Sigecom.Cadastros.Produtos.EditarProduto.Teste
+{
+    public class EdicaoDeProdutoSimplesTeste : BaseTestes
+    {
+        [Test(Description = "Editar produto simples possuindo somente campos obrigatorios")]
+        [AllureTag("CI")]
+        [AllureSeverity(Allure.Commons.SeverityLevel.trivial)]
+        [AllureIssue("1")]
+        [AllureTms("1")]
+        [AllureOwner("Takaki")]
+        [AllureSuite("Cadastros")]
+        [AllureSubSuite("Produto")]
+        public void EditarProdutoSomenteCamposObrigatorios()
+        {
+            // Arange
+            using var beginLifetimeScope = ControleDeInjecaoAutofac.Container.BeginLifetimeScope();
+            var resolveCadastroDeProdutoPage =
+                beginLifetimeScope.Resolve<Func<DriverService, CadastroDeProdutoPage.CadastroDeProdutoBasePage>>();
+            var cadastroDeProdutoPage = resolveCadastroDeProdutoPage(DriverService);
+
+            // Act
+            cadastroDeProdutoPage.EditarProdutoNaTelaDeCadastroDeProduto(cadastroDeProdutoPage);
+            cadastroDeProdutoPage.VerificarCamposDeProduto(TipoDeProduto.Produto);
+            cadastroDeProdutoPage.AcessarAba(CadastroDeProdutoModel.AbaImpostos);
+            cadastroDeProdutoPage.VerificarCamposDeImpostos();
+            cadastroDeProdutoPage.Gravar();
+
+            // Assert
+            cadastroDeProdutoPage.RealizarFluxoDePesquisaDoProduto(cadastroDeProdutoPage, TipoDeProduto.Produto);
+        }
+    }
+}
