433cdd272df3e9a7d74de095dc2b1ece815f198f
Takaki <takaki@sistemasbr.net>
2022-12-07 09:07:38 -0300

    criação(PesquisaDeProduto): Criando testes da pesquisa de produto que faltavam. SIG-CadastroDeProdutoServico

---
+++ SigecomTestesUI/Sigecom/Cadastros/Produtos/PesquisaProduto/Teste/PesquisaDeProdutoPorReferenciaTeste.cs
@@ -0,0 +1,37 @@
+﻿using Autofac;
+using NUnit.Allure.Attributes;
+using NUnit.Framework;
+using SigecomTestesUI.ControleDeInjecao;
+using SigecomTestesUI.Services;
+using SigecomTestesUI.Sigecom.Cadastros.Produtos.PesquisaProduto.Model;
+using System;
+
+namespace SigecomTestesUI.Sigecom.Cadastros.Produtos.PesquisaProduto.Teste
+{
+    public class PesquisaDeProdutoPorReferenciaTeste:BaseTestes
+    {
+        [Test(Description = "Pesquisa de produto pela referencia")]
+        [AllureTag("CI")]
+        [AllureSeverity(Allure.Commons.SeverityLevel.trivial)]
+        [AllureIssue("1")]
+        [AllureTms("1")]
+        [AllureOwner("Takaki")]
+        [AllureSuite("Pesquisa")]
+        [AllureSubSuite("Produto")]
+        public void PesquisarProdutoPelaReferencia()
+        {
+            // Arange
+            using var beginLifetimeScope = ControleDeInjecaoAutofac.Container.BeginLifetimeScope();
+            var resolvePesquisaDeProdutoPage = beginLifetimeScope.Resolve<Func<DriverService, PesquisaDeProdutoPage>>();
+            var pesquisaDeProdutoPage = resolvePesquisaDeProdutoPage(DriverService);
+
+            // Act
+            pesquisaDeProdutoPage.PesquisarComF9UmProdutoNaTelaPrincipal(beginLifetimeScope);
+            pesquisaDeProdutoPage.PesquisarProduto(PesquisaDeProdutoInformacoesParaTesteModel.ReferenciaDoProduto);
+
+            // Assert
+            Assert.True(pesquisaDeProdutoPage.VerificarSeExisteProdutoNaGrid(PesquisaDeProdutoInformacoesParaTesteModel.NomeFinalDoProduto));
+            pesquisaDeProdutoPage.FecharSomenteTelaDePesquisa();
+        }
+    }
+}
