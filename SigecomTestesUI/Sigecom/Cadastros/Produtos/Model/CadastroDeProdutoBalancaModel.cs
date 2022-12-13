c93b4be697e7e8d0fa87b9fc4fc6c872b9568f21
Takaki <takaki@sistemasbr.net>
2022-12-13 18:25:42 -0300

    modificação(Cadastro): Alterando valores do codigo de balança e codigo de barras da grade. SIG-EdicaoDeProduto

--- SigecomTestesUI/Sigecom/Cadastros/Produtos/Model/CadastroDeProdutoBalancaModel.cs
+++ SigecomTestesUI/Sigecom/Cadastros/Produtos/Model/CadastroDeProdutoBalancaModel.cs
@@ -4,7 +4,7 @@
     {
         public static string NomeDoProduto => "PRODUTO BALANCA CADASTRO TESTE";
         public static string CategoriaDoProduto => "BALANCA";
-        public static string CodigoDaBalanca => "000002";
+        public static string CodigoDaBalanca => "000001";
         public static string NomeFinalDoProduto => "PRODUTO BALANCA CADASTRO TESTE";
     }
 }
