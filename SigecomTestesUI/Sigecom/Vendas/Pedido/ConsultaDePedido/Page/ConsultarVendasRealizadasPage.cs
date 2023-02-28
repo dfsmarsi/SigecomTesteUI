c10443af07b933e926e909d82575c73a00a5f0a7
Takaki <takaki@sistemasbr.net>
2023-02-28 15:26:13 -0300

    modificação(testes): Arrumando testes de desconto que estava dando erro no Pc do douglas. SIG-ArrumandoTestes

--- SigecomTestesUI/Sigecom/Vendas/Pedido/ConsultaDePedido/Page/ConsultarVendasRealizadasPage.cs
+++ SigecomTestesUI/Sigecom/Vendas/Pedido/ConsultaDePedido/Page/ConsultarVendasRealizadasPage.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Autofac;
+using NUnit.Framework;
 using SigecomTestesUI.Config;
 using SigecomTestesUI.ControleDeInjecao;
 using SigecomTestesUI.Services;
@@ -42,7 +43,7 @@ namespace SigecomTestesUI.Sigecom.Vendas.Pedido.ConsultaDePedido.Page
         }
 
         private void RealizarFluxoDeVerificarVendasRealizadas() => 
-            DriverService.VerificarSePossuiOValorNaGrid(ConsultaDePedidoModel.CampoDaGridValor, $"R${LancarItemNoPedidoModel.ValorUnitarioParaVerificarVendasRealizadas}");
+            Assert.AreEqual(DriverService.VerificarSePossuiOValorNaTela($"R${LancarItemNoPedidoModel.ValorUnitarioParaVerificarVendasRealizadas}"), true);
 
         private void LancarProdutoPadrao()
         {
