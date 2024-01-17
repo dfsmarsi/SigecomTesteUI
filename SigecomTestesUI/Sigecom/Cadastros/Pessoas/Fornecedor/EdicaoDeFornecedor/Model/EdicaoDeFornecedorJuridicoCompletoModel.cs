cce541148840f43e0f3c288cb7be9cd27a12544d
dfsmarsi <douglas@sistemasbr.net>
2024-01-16 21:06:15 -0300

    alteração teste de edição de fornecedor devido a conflito

--- SigecomTestesUI/Sigecom/Cadastros/Pessoas/Fornecedor/EdicaoDeFornecedor/Model/EdicaoDeFornecedorJuridicoCompletoModel.cs
+++ SigecomTestesUI/Sigecom/Cadastros/Pessoas/Fornecedor/EdicaoDeFornecedor/Model/EdicaoDeFornecedorJuridicoCompletoModel.cs
@@ -2,9 +2,9 @@
 {
     public class EdicaoDeFornecedorJuridicoCompletoModel
     {
-        public static string NomeDoFornecedorAlterado => "SISTEMASBR SOLUCOES EM TECNOLOGIA LTDA";
-        public static string Cnpj => "21075365000155";
-        public static string CnpjComPontos => "21.075.365/0001-55";
+        public static string NomeDoFornecedorAlterado => "B. S. DIGITAL LTDA";
+        public static string Cnpj => "21418205000161";
+        public static string CnpjComPontos => "21.418.205/0001-61";
         public static string Cep => "15700082";
         public static string CepComPontos => "15700-082";
         public static string Endereco => "AVENIDA JOÃO AMADEU";
