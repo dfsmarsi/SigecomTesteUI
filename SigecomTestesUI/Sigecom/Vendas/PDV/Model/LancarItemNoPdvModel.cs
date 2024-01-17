228754f0c489c24c3bebe7cdfa4d31035b4a4d5e
dfsmarsi <douglas@sistemasbr.net>
2024-01-17 17:06:08 -0300

    Ajuste no valor do item para o teste de tabela de preco PDV

--- SigecomTestesUI/Sigecom/Vendas/PDV/Model/LancarItemNoPdvModel.cs
+++ SigecomTestesUI/Sigecom/Vendas/PDV/Model/LancarItemNoPdvModel.cs
@@ -6,7 +6,7 @@
         public static string DescontoNoItemPdv => "5";
         public static string ItemComDescontoNoPdv => "R$5,00";
         public static string ValorUnitarioDoPrimeiroProdutoNoPdv => "R$10,70";
-        public static string ValorUnitarioDoSegundoProdutoNoPdv => "R$10,70";
+        public static string ValorUnitarioDoSegundoProdutoNoPdv => "R$10,50";
         public static string ValorTotalParaVoltarTroco => "100";
     }
 }
